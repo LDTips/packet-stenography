48c60e0f301c8ff663e2451977be12d66cbe45db
Tomasz Zbigniew Czaplicki <t.czaplicki@stud.elka.pw.edu.pl>
2024-04-19 15:17:39 +0200

    UDP receiver

---
+++ receiver_UDP.py
@@ -0,0 +1,36 @@
+import socket
+from scapy.compat import raw
+from scapy.layers.inet import IP, UDP
+from scapy.packet import Raw
+
+UDP_DST_IP='127.0.0.1'
+UDP_SRC_PORT=65123
+UDP_DST_PORT=80
+MSG_SIZE = 64
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((UDP_DST_IP, UDP_DST_PORT))
+
+
+antygona = b''
+def xor(a, b):
+    return bytes([a ^ b for a, b in zip(a, b)])
+
+while True:
+    data, address = sock.recvfrom(1024)
+    if(len(data) == MSG_SIZE):
+        p1 = IP(dst=UDP_DST_IP) / UDP(sport=UDP_SRC_PORT, dport=UDP_DST_PORT) / Raw(load=data)
+        p1 = IP(raw(p1))
+        checksum = hex(p1[UDP].chksum)
+    else:
+        new_xored_data = b''
+        for i in range(MSG_SIZE):
+            new_xored_data += data[i*2].to_bytes(1, 'big')
+        xor_pattern = bytes.fromhex((MSG_SIZE // 2) * checksum[2:])
+        data = xor(new_xored_data, xor_pattern)
+        antygona += data[:64]
+        print(data)
+        if (data.decode('ANSI').__contains__('....')):
+            f = open('odszyfrowane.txt', 'w')
+            f.write(antygona.decode('ANSI'))
+            exit(0)
